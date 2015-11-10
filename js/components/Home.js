f7997ed2d08f2c182ebf5b7b96f6b22a3767def5
koga <koga.rodrigo@gmail.com>
2015-11-10 21:16:09 -0200

    :)

--- js/components/Home.js
+++ js/components/Home.js
@@ -7,7 +7,7 @@ class Home extends React.Component{
     return(
       <div className="view-content">
         <div className="content-title">
-          <h1 className="title"><Type timeout='120' text="Hello! I'm Rodrigo Koga"/><span className="blink">|</span></h1>
+          <h1 className="title"><Type timeout='120' text="Hello! I'm Rodrigo Koga!"/><span className="blink">|</span></h1>
         </div>
         <div className="content">Full-stack Web Developer living in São Paulo, Brazil. <br/>Passionate about new technologies, arts and running.</div>
       </div>
